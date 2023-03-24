0ace25f3246233b043058ac2f5a3a3b969646fa7
Никита <mnd1work@gmail.com>
2023-03-25 01:10:07 +0400

    commit 25.3.23 (1)

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -8,7 +8,17 @@ import { AiOutlineHeart } from 'react-icons/ai';
 const menu = require('../../Menu.json')
 
 class Menu extends React.Component {
+    constructor (props) {
+        super(props) 
+
+        this.state = {
+            favorite_menu: []
+        }
+    }
+
     render () {
+        const { favorite_menu } = this.state;
+
         return (
             <section className="menu">
                 <div className="container">
@@ -21,7 +31,15 @@ class Menu extends React.Component {
                     </button>
 
                     <div className="favorite_container">
-
+                    {
+                        JSON.parse(localStorage.getItem("favorite")).map((item, key) => (
+                            <div className="favorite_card">
+                                <img className="favorite_card_img" src={`./menu/${item.photo}`} alt="" />
+                                <h3 className="favorite_card_header">{item.name}</h3>
+                                <p className="favorite_card_price">{item.price} ₽</p>
+                            </div>
+                        ))
+                    }
                     </div>
 
                     <h3 className="menu_header">{this.props.item}</h3>
@@ -32,6 +50,15 @@ class Menu extends React.Component {
                             if (item.categoty === this.props.item) {
                                 return (
                                     <div className="menu_card">
+                                        <button className="menu_favorite" onClick={() => {
+                                            this.setState({
+                                                JSON: favorite_menu.push(item)
+                                            })
+
+                                            localStorage.setItem("favorite", JSON.stringify(favorite_menu))
+                                        }}>
+                                            <AiOutlineHeart />
+                                        </button>
                                         <img className="menu_card_img" src={`./menu/${item.photo}`} alt="" />
                                         <h3 className="menu_card_header">{item.name}</h3>
                                         <p className="menu_card_weight">{item.weight} гр.</p>
